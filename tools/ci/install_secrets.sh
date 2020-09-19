7ccf82a6eb50aa22a4bf954005c3b8a883f94398
Michel Tricot <michel@dataline.io>
2020-09-19 13:32:21 -0700

    progress

---
+++ tools/ci/install_secrets.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+WORKDIR=$(mktemp -d)
+APP=git-crypt-0.6.0
+SOURCES=https://www.agwa.name/projects/git-crypt/downloads/$APP.tar.gz
+TARBALL=$(basename $SOURCES)
+
+cp tools/ci/${TARBALL}.asc "$WORKDIR"
+cd $WORKDIR
+wget $SOURCES
+#gpg --import ${TARBALL}.asc
+#gpg --verify ${TARBALL}.asc $TARBALL
+tar  zxf $TARBALL
+cd $APP
+make
+
+#echo "$GPG_KEY" > private.key
+#echo "$GPG_PASSPHRASE" | gpg --batch --yes --passphrase-fd 0 --import private.key
+#rm private.key
+#gpg --list-secret-keys --keyid-format LONG
