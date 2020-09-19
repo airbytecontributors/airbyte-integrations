be0434169b31afe0bdfaa544f86826ef2f257235
Michel Tricot <michel@dataline.io>
2020-09-19 14:33:14 -0700

    update

--- tools/ci/install_secrets.sh
+++ tools/ci/install_secrets.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 . tools/lib/lib.sh
 
@@ -10,7 +10,13 @@ GIT_CRYPT_BIN=$WORKDIR/$GIT_CRYPT_RELEASE/git-crypt
 
 function install_git-crypt() {
   local tarball=$GIT_CRYPT_RELEASE.tar.gz
+
+  cp tools/ci/$tarball.asc $WORKDIR
+
+  cd $WORKDIR
   wget https://www.agwa.name/projects/git-crypt/downloads/$tarball
+  gpg --verify $tarball.asc $tarball
+
   tar zxf $tarball
   cd $GIT_CRYPT_RELEASE
   make
@@ -25,7 +31,8 @@ function install_gpg_key() {
 #gpg --import ${TARBALL}.asc
 #gpg --verify ${TARBALL}.asc $TARBALL
 
-( cd $WORKDIR && install_git-crypt )
+
+( install_git-crypt ) # we are cd-ing during install, let's make sure we don't loose the current path
 install_gpg_key
 
 [ "$(cat secrets/encryption_probe)" != "decrypted" ] || error "Secret shouldn't be visible. Something very wrong is happening."
