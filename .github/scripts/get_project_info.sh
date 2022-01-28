c5e178dc79f3fa88dc3155696cf9c0d051409f24
pmossman <parker@airbyte.io>
2022-01-28 10:18:56 -0800

    misc scripts

---
+++ .github/scripts/get_project_info.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+my_org="airbytehq"
+project_id="PN_kwDOA4_XW84AAj7t"
+
+gh api graphql -f query='
+  query($project_id: ID!) {
+    node(id: $project_id) {
+      ... on ProjectNext {
+        fields(first: 100) {
+          nodes {
+            id
+            name
+            settings
+          }
+        }
+      }
+    }
+  }' -f project_id=$project_id
