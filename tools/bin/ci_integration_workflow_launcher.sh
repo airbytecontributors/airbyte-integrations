ee3c01c5adc497027767863265cbca29a8f7ab9f
grishick <greg@airbyte.io>
2022-03-31 19:28:57 -0700

    try running just source-postgres

--- tools/bin/ci_integration_workflow_launcher.sh
+++ tools/bin/ci_integration_workflow_launcher.sh
@@ -27,14 +27,14 @@ if [ "$RUNNING_MASTER_WORKFLOWS" -gt "$MAX_RUNNING_MASTER_WORKFLOWS" ]; then
   exit 0
 fi
 
-CONNECTORS=$(./gradlew integrationTest --dry-run | grep 'integrationTest SKIPPED' | cut -d: -f 4 | sort | uniq)
-echo "$CONNECTORS" | while read -r connector; do
-  echo "Issuing request for connector $connector..."
+#CONNECTORS=$(./gradlew integrationTest --dry-run | grep 'integrationTest SKIPPED' | cut -d: -f 4 | sort | uniq)
+#echo "$CONNECTORS" | while read -r connector; do
+  echo "Issuing request for connector source-postgres..."
   curl \
     -i \
     -X POST \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: Bearer $GITHUB_TOKEN" \
     "$REPO_API/actions/workflows/$WORKFLOW_ID/dispatches" \
-    -d "{\"ref\":\"master\", \"inputs\": { \"connector\": \"$connector\"} }"
+    -d "{\"ref\":\"master\", \"inputs\": { \"connector\": \"source-postgres\"} }"
 done
