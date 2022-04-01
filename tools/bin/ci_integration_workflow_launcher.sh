a6ae01c95f2a09032907e830ed31d9cd4a8b7947
grishick <greg@airbyte.io>
2022-04-01 12:10:03 -0700

    use GITHUB_REF env variable

--- tools/bin/ci_integration_workflow_launcher.sh
+++ tools/bin/ci_integration_workflow_launcher.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-# launches integration test workflows for master builds
+# launches integration test workflows
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
   echo "GITHUB_TOKEN not set..."
   exit 1
 fi
+
+echo "Launching integration tests on $GITHUB_REF"
 
 REPO_API=https://api.github.com/repos/airbytehq/airbyte
 WORKFLOW_PATH=.github/workflows/test-command.yml
@@ -19,10 +21,10 @@ if [ "$MATCHING_WORKFLOW_IDS" -ne "1" ]; then
   exit 1
 fi
 
-MAX_RUNNING_MASTER_WORKFLOWS=5
-RUNNING_MASTER_WORKFLOWS=$(curl "$REPO_API/actions/workflows/$WORKFLOW_ID/runs?branch=master&status=in_progress" --header "Authorization: Bearer $GITHUB_TOKEN" | jq -r ".total_count")
-if [ "$RUNNING_MASTER_WORKFLOWS" -gt "$MAX_RUNNING_MASTER_WORKFLOWS" ]; then
-  echo "More than $MAX_RUNNING_MASTER_WORKFLOWS integration tests workflows running on master."
+MAX_RUNNING_WORKFLOWS=5
+RUNNING_WORKFLOWS=$(curl "$REPO_API/actions/workflows/$WORKFLOW_ID/runs?branch=$GITHUB_REF_NAME&status=in_progress" --header "Authorization: Bearer $GITHUB_TOKEN" | jq -r ".total_count")
+if [ "$RUNNING_WORKFLOWS" -gt "$MAX_RUNNING_WORKFLOWS" ]; then
+  echo "More than $MAX_RUNNING_WORKFLOWS integration tests workflows running on $GITHUB_REF_NAME."
   echo "Skipping launching workflows."
   exit 0
 fi
@@ -36,5 +38,5 @@ echo "$CONNECTORS" | while read -r connector; do
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: Bearer $GITHUB_TOKEN" \
     "$REPO_API/actions/workflows/$WORKFLOW_ID/dispatches" \
-    -d "{\"ref\":\"master\", \"inputs\": { \"connector\": \"$connector\"} }"
+    -d "{\"ref\":\"$GITHUB_REF_NAME\", \"inputs\": { \"connector\": \"$connector\", \"gitref\": \"$GITHUB_REF_NAME\"} }"
 done
