070d59713e0ad3fff6b480872fb0e0f227adc12a
tirth <tirth@bicycle.io>
2022-06-15 17:42:00 +0530

    Made some changes to BaseEventConnector, modified KafkaConnector to look like StandAloneConnector

---
+++ airbyte-integrations/bases/base-bicycle-integration/src/main/java/io/airbyte/integrations/bicycle/base/integration/BicycleAuthInfo.java
@@ -0,0 +1,45 @@
+package io.airbyte.integrations.bicycle.base.integration;
+
+import com.inception.server.auth.model.AuthInfo;
+import com.inception.server.auth.model.AuthType;
+import com.inception.server.auth.model.Principal;
+
+public class BicycleAuthInfo implements AuthInfo {
+    private final String token;
+    private final String tenantId;
+
+    public BicycleAuthInfo(String token, String tenantId) {
+        this.token = token;
+        this.tenantId = tenantId;
+    }
+
+    @Override
+    public Principal getPrincipal() {
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        return token;
+    }
+
+    @Override
+    public String getTenantCode() {
+        return null;
+    }
+
+    @Override
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    @Override
+    public String getAccountId() {
+        return null;
+    }
+
+    @Override
+    public AuthType getAuthType() {
+        return AuthType.BEARER;
+    }
+}
