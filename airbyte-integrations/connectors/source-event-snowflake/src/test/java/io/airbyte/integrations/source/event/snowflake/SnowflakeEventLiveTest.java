0f372068d94161c9007d956d14eaf3b50737d05d
airbytecontributors <106863490+airbytecontributors@users.noreply.github.com>
2024-04-05 17:11:55 +0530

    Snowflake connector fixes (#108)
    * Updating spec for big query to include sync mode
    
    * Changes for backfill in snowflake
    
    ---------
    
    Co-authored-by: sumit <sumit@inceptioncloud.io>

--- airbyte-integrations/connectors/source-event-snowflake/src/test/java/io/airbyte/integrations/source/event/snowflake/SnowflakeEventLiveTest.java
+++ airbyte-integrations/connectors/source-event-snowflake/src/test/java/io/airbyte/integrations/source/event/snowflake/SnowflakeEventLiveTest.java
@@ -3,6 +3,7 @@ package io.airbyte.integrations.source.event.snowflake;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inception.server.auth.api.SystemAuthenticator;
+import com.inception.server.auth.model.AuthInfo;
 import io.airbyte.commons.util.AutoCloseableIterator;
 import io.airbyte.integrations.bicycle.base.integration.EventConnectorJobStatusNotifier;
 import io.airbyte.integrations.source.event.snowflake.SnowflakeEventSource;
@@ -10,6 +11,9 @@ import io.airbyte.protocol.models.AirbyteCatalog;
 import io.airbyte.protocol.models.AirbyteMessage;
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog;
 import io.bicycle.event.processor.api.BicycleEventProcessor;
+import io.bicycle.integration.common.config.manager.ConnectorConfigManager;
+import io.bicycle.integration.connector.runtime.BackFillConfiguration;
+import io.bicycle.integration.connector.runtime.RuntimeConfig;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -24,6 +28,51 @@ public class SnowflakeEventLiveTest {
 
     public static void main(String[] args) throws Exception {
 
+        //testWithoutBackFill();
+        testWithBackFill();
+
+    }
+
+    public static void testWithBackFill() throws Exception {
+
+
+        RuntimeConfig runtimeConfig = RuntimeConfig.newBuilder()
+                .setBackFillConfig(BackFillConfiguration.newBuilder().setEnableBackFill(true)
+                        .setEndTimeInMillis(10000).build()).build();
+
+
+        ConnectorConfigManager connectorConfigManager = Mockito.mock(ConnectorConfigManager.class);
+        Mockito.when(connectorConfigManager.getRuntimeConfig(Mockito.any(AuthInfo.class),
+                Mockito.any(String.class))).thenReturn(runtimeConfig);
+
+        SnowflakeEventSource snowFlakeEventSource = new SnowflakeEventSource(Mockito.mock(SystemAuthenticator.class),
+                Mockito.mock(EventConnectorJobStatusNotifier.class), connectorConfigManager);
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String configString = readFileAsString("snowflake-sumit-backfill-config.json");
+        JsonNode config = objectMapper.readValue(configString, JsonNode.class);
+
+        // System.out.println(snowFlakeEventSource.check(config));
+
+        AirbyteCatalog catalog = snowFlakeEventSource.discover(config);
+        //  System.out.println(catalog);
+
+        System.out.println(objectMapper.writeValueAsString(catalog.getStreams().get(0)));
+
+        String catalogString = readFileAsString("snowflake-sumit-backfill-catalog.json");
+        ConfiguredAirbyteCatalog configuredAirbyteCatalog = objectMapper.readValue(catalogString, ConfiguredAirbyteCatalog.class);
+     /*   AutoCloseableIterator<AirbyteMessage> iterator =
+                snowFlakeEventSource.preview(config, configuredAirbyteCatalog, null);
+        System.out.println(iterator.next());*/
+
+        snowFlakeEventSource.read(config, configuredAirbyteCatalog, null);
+
+
+    }
+
+    public static void testWithoutBackFill() throws Exception {
+
         SnowflakeEventSource snowFlakeEventSource = new SnowflakeEventSource(Mockito.mock(SystemAuthenticator.class),
                 Mockito.mock(EventConnectorJobStatusNotifier.class), null);
 
@@ -32,10 +81,10 @@ public class SnowflakeEventLiveTest {
         String configString = readFileAsString("snowflake-sumit-config.json");
         JsonNode config = objectMapper.readValue(configString, JsonNode.class);
 
-       // System.out.println(snowFlakeEventSource.check(config));
+        // System.out.println(snowFlakeEventSource.check(config));
 
         AirbyteCatalog catalog = snowFlakeEventSource.discover(config);
-      //  System.out.println(catalog);
+        //  System.out.println(catalog);
 
         System.out.println(objectMapper.writeValueAsString(catalog.getStreams().get(0)));
 
@@ -46,7 +95,6 @@ public class SnowflakeEventLiveTest {
         System.out.println(iterator.next());*/
 
         snowFlakeEventSource.read(config, configuredAirbyteCatalog, null);
-
 
 
     }
